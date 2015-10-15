30b53511fb3d1e2e2f2ec6a1158915b0ff092572
beancoding <dmcliver@gmail.com>
2015-10-16 08:48:01 +1300

    Added config properties file

--- src/main/java/com/dmcliver/donateme/AppConfig.java
+++ src/main/java/com/dmcliver/donateme/AppConfig.java
@@ -3,8 +3,11 @@ package com.dmcliver.donateme;
 import java.beans.PropertyVetoException;
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,8 +16,12 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:/config.properties")
 public class AppConfig {
 
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public HibernateTransactionManager transactionManager() throws PropertyVetoException{
 		HibernateTransactionManager tx = new HibernateTransactionManager(sessionFactory().getObject());
@@ -34,7 +41,11 @@ public class AppConfig {
 	public Properties properties() {
 
 		Properties props = new Properties();
-		//props.setProperty("hibernate.hbm2ddl.auto", "create");
+		
+		String dbTask = env.getProperty("db.hbm2ddl.auto", "");
+		if(!"".equals(dbTask))
+			props.setProperty("hibernate.hbm2ddl.auto", dbTask);
+		
 		props.setProperty("hibernate.show_sql", "true");
 		return props;
 	}
@@ -43,10 +54,10 @@ public class AppConfig {
 	public ComboPooledDataSource dataSource() throws PropertyVetoException {
 
 		ComboPooledDataSource ds = new ComboPooledDataSource();
-		ds.setJdbcUrl("jdbc:postgresql://localhost:5432/DonateMeDb");
-		ds.setDriverClass("org.postgresql.Driver");
-		ds.setUser("postgres");
-		ds.setPassword("root");
+		ds.setJdbcUrl(env.getProperty("db.url"));
+		ds.setDriverClass(env.getProperty("db.driver"));
+		ds.setUser(env.getProperty("db.user"));
+		ds.setPassword(env.getProperty("db.password"));
 		return ds;
 	}
 }
