80e21220fd4c45f4779fa1ccb9dbe91f2f1eb1f9
me <dmcliver@gmail.com>
2016-01-27 23:33:06 +1300

    Added Brand entity

---
+++ src/main/java/com/dmcliver/donateme/domain/Brand.java
@@ -0,0 +1,36 @@
+package com.dmcliver.donateme.domain;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "Brand")
+public class Brand {
+
+	private UUID brandId;
+	private String BrandName;
+	
+	@Id
+	@Type(type = "pg-uuid")
+	@Column(name = "BrandId")
+	public UUID getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(UUID brandId) {
+		this.brandId = brandId;
+	}
+	
+	@Column(name = "BrandName", nullable = false)
+	public String getBrandName() {
+		return BrandName;
+	}
+	public void setBrandName(String brandName) {
+		BrandName = brandName;
+	}
+}
